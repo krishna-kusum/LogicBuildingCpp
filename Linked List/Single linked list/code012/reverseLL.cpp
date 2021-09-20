db529e493625977120c35521e5d9e26d3c869fe1
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2021-09-20 15:23:02 +0530

    Create reverseLL.cpp

---
+++ Linked List/Single linked list/code012/reverseLL.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+
+class node{
+    
+    public:
+    int data;
+    node* next;
+    
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+//insert at head 
+void insertAtHead(int data, node* &head){
+    node* newnode = new node(data);
+    newnode->next = head;
+    head = newnode;
+}
+
+//insert at tail
+
+void insertAtTail(int data, node* &head){
+    node* newnode = new node(data);
+    if(head == NULL){
+        head = newnode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    
+}
+
+//deleteAtPos
+void deleteAtPos(node* &head, int pos){
+    
+    if(head == NULL){
+        return;
+    }
+    
+    int count = 1;
+    
+    node* temp = head;
+    while(count<pos-1){
+        temp = temp->next;
+        count++;
+        if(temp == NULL){
+        return;
+    }
+    }
+    
+    node* deleteNode = temp->next;
+    
+    temp->next = temp->next->next;
+    delete deleteNode;
+}
+
+//deletionAtBegin
+void deleteAtStart(node* &head){
+    node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+//delete at end
+void deleteAtEnd(node* &head){
+    
+    node* temp = head;
+    
+    while(temp->next->next){
+        temp = temp->next;
+        
+    }
+    
+    node* deleteNode = temp->next;
+    temp->next = NULL;
+    delete deleteNode;
+    
+}
+
+
+void display(node* head){
+    
+    node* temp = head;
+    while(temp != NULL){
+        std::cout << temp->data << std::endl;
+        temp = temp->next;
+        
+    }
+}
+
+//reversing the linked List
+void reverse(node* &head){
+    node* current = head;
+    node* nextPtr = NULL;
+    node* prev = NULL;
+    
+    while(current != NULL){
+        nextPtr = current->next;
+        current->next = prev;
+        
+        prev = current;
+        current = nextPtr;
+    }
+    
+    head = prev;
+}
+
+int main(){
+    
+    node* head = new node(1);
+    insertAtTail(2,head);
+    insertAtTail(3,head);
+    
+    insertAtHead(4,head);
+    
+    reverse(head);
+    display(head);
+    return 0;
+}
