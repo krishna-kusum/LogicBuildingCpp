b61e6147d138fa1fe97e3cf7efc585fc206fec02
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2021-09-20 13:28:54 +0530

    Create deleteAtPos.cpp

---
+++ Linked List/Single linked list/code011/deleteAtPos.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+
+class node{
+    
+    public:
+    int data;
+    node* next;
+    
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+//insert at head 
+void insertAtHead(int data, node* &head){
+    node* newnode = new node(data);
+    newnode->next = head;
+    head = newnode;
+}
+
+//insert at tail
+
+void insertAtTail(int data, node* &head){
+    node* newnode = new node(data);
+    if(head == NULL){
+        head = newnode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    
+}
+
+//deleteAtPos
+void deleteAtPos(node* &head, int pos){
+    
+    if(head == NULL){
+        return;
+    }
+    
+    int count = 1;
+    
+    node* temp = head;
+    while(count<pos-1){
+        temp = temp->next;
+        count++;
+        if(temp == NULL){
+        return;
+    }
+    }
+    
+    node* deleteNode = temp->next;
+    
+    temp->next = temp->next->next;
+    delete deleteNode;
+}
+
+void display(node* head){
+    
+    node* temp = head;
+    while(temp != NULL){
+        std::cout << temp->data << std::endl;
+        temp = temp->next;
+        
+    }
+}
+
+int main(){
+    
+    node* head = new node(1);
+    insertAtTail(2,head);
+    insertAtTail(3,head);
+    
+    insertAtHead(4,head);
+    
+    display(head);
+    std::cout<<"\n";
+    deleteAtPos(head, 3);
+    display(head);
+    
+    return 0;
+}
