0a2b671d7c9319dfdecaf894f3ae64a397fbe5a6
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2021-09-20 12:30:08 +0530

    Update insertAtTail.cpp

--- Linked List/Single linked list/code010/insertAtTail.cpp
+++ Linked List/Single linked list/code010/insertAtTail.cpp
@@ -3,54 +3,55 @@
 class node{
     
     public:
-        int data;
-        node* next;
-        
+    int data;
+    node* next;
+    
     node(int val){
         data = val;
         next = NULL;
     }
 };
 
+//insert at head 
+void insertAtHead(int data, node* &head){
+    node* newnode = new node(data);
+    newnode->next = head;
+    head = newnode;
+}
+
 //insert at tail
-void insertAtTail(node* &head, int data){
-     node* newnode = new node(data);
-    
+
+void insertAtTail(int data, node* &head){
+    node* newnode = new node(data);
     if(head == NULL){
         head = newnode;
         return;
     }
     
-    
     node* temp = head;
-    
     while(temp->next != NULL){
         temp = temp->next;
     }
-    
     temp->next = newnode;
+    
 }
 
-
-//display
-void display(node* ptr){
+void display(node* head){
     
-    while(ptr != NULL){
-        std::cout << ptr->data << " ";
-        ptr = ptr->next;
+    node* temp = head;
+    while(temp != NULL){
+        std::cout << temp->data << std::endl;
+        temp = temp->next;
     }
 }
 
-
 int main(){
-    node* head = new node(9); 
     
-    insertAtTail(head, 8);
-    insertAtTail(head, 8);
-    insertAtTail(head, 9);
+    node* head = new node(1);
+    insertAtTail(2,head);
+    insertAtTail(3,head);
+    insertAtHead(4,head);
     
     display(head);
-    
-    
     return 0;
 }
