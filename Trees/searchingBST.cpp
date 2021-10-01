f88aa1649281c1fd7222724f4e96c6ac5a137d4c
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2021-10-01 08:34:01 +0530

    Create searchingBST.cpp

---
+++ Trees/searchingBST.cpp
@@ -0,0 +1,51 @@
+//Binary Search Tree
+#include<iostream>
+
+//tree node
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+    
+    node(int val){
+        this->data = val;
+    
+        left = NULL;
+        right = NULL;
+    }
+};
+
+//searching 
+struct node* search(struct node* root, int val){
+    
+    //if found or reached NULL
+    if(root ==  NULL || root->data == val ){
+        return root;
+    }
+    
+    //if val is less than root->data; search leftsubtree
+    if(val < root->data){
+        return search(root->left, val);
+    }
+    
+    //if val is greater than root->data; search rightsubtree
+    if(val > root->data){
+        return search(root->right, val);
+    }
+}
+
+//driver function
+int main(){
+    
+    struct node* root = new node(4);
+    root->left = new node(2);
+    root->right = new node(6);
+    root->left->left = new node(1);
+    root->left->right = new node(3);
+    root->right->left = new node(5);
+    
+    struct node* found = search(root, 6);
+    std::cout<<found->left->data<<" "<<found->data<<" "<<found->right->data;
+    
+    return 0;
+}
